bf0cc272deb4ef48ea7c5eb0628b2214aedb9fb1
ぱら <eriglai577@gmail.com>
2019-12-24 18:25:24 +0900

    新規作成

---
+++ YoineLogicTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+class YoineLogicTest {
+	YoineLogic yl = new YoineLogic();
+
+	@Test
+	void YoinePlusメソッド呼び出すテスト() {
+		yl.yoinePlus();
+	}
+
+}
